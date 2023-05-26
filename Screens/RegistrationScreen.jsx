8b9a798adc539abd88be992c3cb66e5afd002e22
tekostyshyn <tekostyshyn@gmail.com>
2023-05-26 21:16:40 +0300

    reg screen form fields styled

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import BackgroundImage from "../images/background-image.jpeg";
 
-import { ImageBackground, StyleSheet, View, Animated, Text } from "react-native";
+import { ImageBackground, StyleSheet, View, Animated, Text, TextInput } from "react-native";
+
+const onFocusStyle = {borderColor: "#FF6C00", color: "#212121", backgroundColor: "#FFFFFF"};
+const onBlurStyle = {borderColor: "#E8E8E8", color: '#BDBDBD', backgroundColor: "#F6F6F6"};
 
 const RegistrationScreen = () => {
+  const [name, onChangeName] = React.useState("");
+  const [number, onChangeNumber] = React.useState("");
+  const [nameInputStyles, setNameInputStyles] = React.useState({...onBlurStyle});
+  const [numberInputStyles, setNumberInputStyles] = React.useState({...onBlurStyle});
+  const [passwordInputStyles, setPasswordInputStyles] = React.useState({...onBlurStyle});
+
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -15,7 +25,33 @@ const RegistrationScreen = () => {
       >
         <View style={styles.box}>
           <Animated.View style={styles.imageWrapper}></Animated.View>
-          <Text>Реєстрація</Text>
+          <Text style={styles.title}>Реєстрація</Text>
+          <TextInput
+            style={[styles.input, nameInputStyles]}
+            onChangeText={onChangeName}
+            value={name}
+            placeholder="Логін"
+            onFocus={() => {setNameInputStyles({...onFocusStyle})}}
+            onBlur={() => {setNameInputStyles({...onBlurStyle})}}
+          />
+          <TextInput
+            style={[styles.input, numberInputStyles]}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Адреса електронної пошти"
+            keyboardType="numeric"
+            onFocus={() => {setNumberInputStyles({...onFocusStyle})}}
+            onBlur={() => {setNumberInputStyles({...onBlurStyle})}}
+          />
+             <TextInput
+            style={[styles.input, passwordInputStyles]}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Пароль"
+            keyboardType="numeric"
+            onFocus={() => {setPasswordInputStyles({...onFocusStyle})}}
+            onBlur={() => {setPasswordInputStyles({...onBlurStyle})}}
+          />
         </View>
       </ImageBackground>
     </View>
@@ -46,6 +82,28 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     width: 120,
     height: 120,
+  },
+  title: {
+    // font-family: Roboto;
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: 500,
+    lineHeight: 1.17,
+    marginBottom: 33,
+  },
+  input: {
+    // font-family: Roboto;
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderRadius: 8,
+    width: 343,
+    height: 50,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: 16,
+    padding: 16,
+    fontSize: 16,
+    lineHeight: 1.17,
   },
 });
 
