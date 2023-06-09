85de1ff1168913cd2ef3ef47efcdd762ce09a5a0
tekostyshyn <tekostyshyn@gmail.com>
2023-06-09 16:29:52 +0300

    Maps added

--- Screens/MapScreen.jsx
+++ Screens/MapScreen.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet, View } from "react-native";
 
 const MapScreen = () => {
-    return <></>
-}
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 48.91527,
+          longitude: 24.725197,
+          latitudeDelta: 0.082,
+          longitudeDelta: 0.0421,
+        }}
+        mapType="standard"
+      >
+        <Marker
+          title="I am here"
+          coordinate={{ latitude: 48.91527, longitude: 24.725197 }}
+          description="Hello"
+        />
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    paddingTop: 32,
+    paddingBottom: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default MapScreen;
